d1ed03f122c810ba774a9203c49ef2d35f829dc6
andyBzg <andr.bzg@gmail.com>
2022-10-17 22:53:53 +0200

    Commit

---
+++ src/Homework_11_10.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Homework_11_10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Level 1
+        int number = scanner.nextInt();
+        if (number % 2 == 0) {
+            System.out.println(number + 2);
+        }
+        else {
+            System.out.println(number + 1);
+        }
+
+        //Level 2
+        int time = scanner.nextInt();
+        int trainSpeed = scanner.nextInt();
+        int roadLength = scanner.nextInt();
+        int howFarIsJohn = time * trainSpeed;
+
+        if (howFarIsJohn > roadLength / 2) {
+            System.out.println("Мери");
+        }
+        else {
+            System.out.println("Шерлок");
+        }
+    }
+}
