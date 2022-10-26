3d4fa51eb76276a987d2bd8ef89bc1455c88e530
andyBzg <andr.bzg@gmail.com>
2022-10-26 23:46:13 +0200

    Commit

---
+++ src/Homework_20_10.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Homework_20_10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //Level 1
+        System.out.println("Найдем сумму такого выражения: 1+1/2+1/3+1/4…..+1/n");
+        System.out.print("Введите n: ");
+        int firstNumber = scanner.nextInt();
+        double sum = 0;
+        for (double i = 1; i <= firstNumber; i++) {
+            sum += 1 / i;
+        }
+        System.out.println("Ответ: " + sum);
+
+        //Level 2
+        System.out.println("Введите число, которое вмещается в int: ");
+        int secondNumber = scanner.nextInt();
+        int answer = 0;
+        while (secondNumber > 0) {
+            int i = 10;
+            answer += secondNumber % 10;
+            secondNumber /= i;
+        }
+        System.out.println("Сумма всех цифр этого числа = " + answer);
+    }
+}
